555c76b9897f51a1d97c057f5118497eff7246d3
Quitto <brunamoreiraquitto@gmail.com>
2025-08-08 08:28:04 -0300

    Lauch

---
+++ index.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "Uso: " << argv[0] << " <nome_arquivo>\n";
+        return 1;
+    }
+
+    std::string nameFile = argv[1];
+    
+    std::ofstream File(nameFile);
+    
+    if (!File){
+        std::cerr << "Erro ao criar o arquivo: " << nameFile << "\n";
+        return 0;
+    }
+
+    return 0;
+}
---
+++ touch.bat
@@ -0,0 +1,2 @@
+@echo off
+start "%~dp0touch.exe" %*
